7ae1c13b0b450dc53f1cd84d0fb7483f66a138fd
Matthew Bolan <Bolan.Matthew.MB@Gmail.com>
2021-09-01 08:50:14 -0500

    first commit

---
+++ BA.py
@@ -0,0 +1,64 @@
+
+class BankAccount:
+
+
+    def __init__(self, int_rate, balance):
+
+        self.int_rate = int_rate
+
+        self.balance = balance
+
+
+    def deposit(self, amount):
+
+        self.balance += amount
+
+        return self
+
+
+
+    def withdraw(self, amount):
+
+            if(self.balance - amount) >= 0:
+
+                self.balance -= amount
+
+            else:
+
+                print("Insufficient Funds: Charging a $5 fee")
+
+                self.balance -= 5
+
+            return self
+
+
+
+
+    def display_account_info(self):
+
+                print(f" Balance: {self.balance} ")
+
+                return self
+
+
+
+
+    def yield_interest(self):
+
+            if self.balance > 0:
+
+                self.balance += (self.balance * self.int_rate)
+
+            return self
+
+
+
+
+
+savings = BankAccount (.18,2500)
+
+checking = BankAccount (.09,1250)
+
+savings.deposit(25).deposit(45).deposit(15).withdraw(50).yield_interest().display_account_info()
+
+checking.deposit(165).deposit(35).withdraw(23).withdraw(26).withdraw(75).withdraw(85).yield_interest().display_account_info()
