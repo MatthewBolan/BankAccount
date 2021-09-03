fe3b1b9f2cd745fcb913167451fc4d8a92e5c57f
Matthew Bolan <Bolan.Matthew.MB@Gmail.com>
2021-09-03 07:48:37 -0500

    Spacing&Etc.

--- BA.py
+++ BA.py
@@ -1,55 +1,65 @@
-
 class BankAccount:
+
+    accounts=[]
+
+
 
 
     def __init__(self, int_rate, balance):
 
-        self.int_rate = int_rate
+        self.int_rate=int_rate
 
-        self.balance = balance
+        self.balance=balance
+
+        BankAccount.accounts.append(self)
 
 
     def deposit(self, amount):
 
-        self.balance += amount
+        self.balance+=amount
 
         return self
 
 
-
     def withdraw(self, amount):
 
-            if(self.balance - amount) >= 0:
+            if(self.balance-amount)>=0:
 
-                self.balance -= amount
+                self.balance-=amount
 
             else:
 
                 print("Insufficient Funds: Charging a $5 fee")
 
-                self.balance -= 5
+                self.balance-=5
 
             return self
-
-
 
 
     def display_account_info(self):
 
-                print(f" Balance: {self.balance} ")
+                print(f"Balance:{self.balance}")
 
                 return self
 
 
-
-
     def yield_interest(self):
 
-            if self.balance > 0:
+            if self.balance>0:
 
-                self.balance += (self.balance * self.int_rate)
+                self.balance+=(self.balance*self.int_rate)
 
             return self
+
+
+#    @classmethod
+    
+#    def print_all_accounts(cls):
+        
+#        for account in cls.accounts:
+            
+#            account.display_account_info()
+
 
 
 
